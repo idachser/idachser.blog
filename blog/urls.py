e8c7056471646a789165f274797d4b440abe395a
dachser <idachser@proton.me>
2025-10-30 18:59:23 +0100

    add tagget posts list view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.posts_list, name="posts"),
     path("tags/", views.tags_list, name="tags"),
+    path("tags/<slug:tag_slug>", views.tagged_posts_list, name="tagged_posts"),
     path("<str:slug>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,20 @@ from .models import Post, Tag
 
 def posts_list(request):
     md = markdown.Markdown(extensions=["fenced_code", "codehilite"])
-    posts = get_list_or_404(Post)
+    posts = Post.objects.filter(published=True).order_by("-publish_date")
     for post in posts:
         post.description = md.convert(post.description)
     return render(request, "blog/posts.html", {"posts": posts})
+
+
+def tagged_posts_list(request, tag_slug):
+    md = markdown.Markdown(extensions=["fenced_code", "codehilite"])
+    tag = get_object_or_404(Tag, name=tag_slug)
+    posts = get_list_or_404(Post, tags=tag, published=True)
+    for post in posts:
+        post.description = md.convert(post.description)
+    context = {"tag": tag, "posts": posts}
+    return render(request, "blog/posts.html", context=context)
 
 
 def post_detail(request, slug):
