19a5ff4a8a6130d4da66317f8a207810783a99d2
dachser <idachser@proton.me>
2025-12-05 16:51:44 +0100

    add pull news from golem and spectrum

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,25 @@
-FROM ghcr.io/astral-sh/uv:python3.12-bookworm-slim
+FROM ghcr.io/astral-sh/uv:python3.12-trixie-slim
+
+RUN useradd -m appuser
 
 WORKDIR /app
+RUN chown -R appuser:appuser /app
+
+USER appuser
 
 ENV UV_COMPILE_BYTECODE=1
 ENV UV_LINK_MODE=copy
 ENV UV_TOOL_BIN_DIR=/usr/local/bin
 ENV PYTHONUNBUFFERED=1
 
-RUN --mount=type=cache,target=/root/.cache/uv \
+RUN --mount=type=cache,target=/home/appuser/.cache/uv,uid=1000,gid=1000 \
     --mount=type=bind,source=uv.lock,target=uv.lock \
     --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
     uv sync --locked --no-install-project --no-dev
 
-COPY . /app
+COPY --chown=appuser:appuser . /app
 
-RUN --mount=type=cache,target=/root/.cache/uv \
+RUN --mount=type=cache,target=/home/appuser/.cache/uv,uid=1000,gid=1000 \
     uv sync --locked --no-dev
 
 ENV PATH="/app/.venv/bin:$PATH"
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime as dt
+
+import feedparser
 import markdown
 from django.shortcuts import get_object_or_404, render
 
@@ -12,4 +15,36 @@ def about(request):
 
 
 def news(request):
-    return render(request, "pages/news.html")
+    it_news = feedparser.parse("https://rss.golem.de/rss.php?feed=ATOM1.0")
+    sc_news = feedparser.parse(
+        "https://www.spektrum.de/alias/rss/spektrum-de-rss-feed/996406"
+    )
+
+    news = []
+    for entry in it_news.entries:
+        new = {
+            "title": entry.title,
+            "pub_date": dt.strptime(f"{entry.published}", "%Y-%m-%dT%H:%M:%S%z"),
+            "desc": entry.summary,
+            "link": entry.link,
+            "author": entry.author,
+        }
+        news.append(new)
+    for entry in sc_news.entries:
+        news.append(
+            {
+                "title": entry.title,
+                "pub_date": dt.strptime(
+                    f"{entry.published}", "%a, %d %b %Y %H:%M:%S %z"
+                ),
+                "desc": entry.summary,
+                "link": entry.link,
+                "author": entry.author if hasattr(entry, "author") else "Unknown",
+                "categories": [tag["term"] for tag in getattr(entry, "tags", [])[:3]],
+            }
+        )
+
+    context = {
+        "news": news,
+    }
+    return render(request, "pages/news.html", context=context)
